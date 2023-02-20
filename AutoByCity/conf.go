8020435bf77dd2044e87dad976f2d5b82056b657
GuanEr15 <guaner15@163.com>
2023-02-20 12:43:05 +0800

    edit Code01_SelectionSort.java

--- AutoByCity/conf.go
+++ AutoByCity/conf.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v3"
 	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Conf struct {
-	SourceData	 string `yaml:"SourceData"`
+	SourceData   string `yaml:"SourceData"`
 	SourceSheets string `yaml:"SourceSheets"`
-	ToSheets	 string	`yaml:"ToSheets"`
-	SplitByForm  string	`yaml:"SplitByForm"`
+	ToSheets     string `yaml:"ToSheets"`
+	SplitByForm  string `yaml:"SplitByForm"`
 }
 
-func ReadYaml()  *Conf{
+func ReadYaml() *Conf {
 	// 读取配置文件
 	var conf Conf
-	configs,err := ioutil.ReadFile("./configs/conf.yaml")
-	if err != nil{
+	configs, err := ioutil.ReadFile("./configs/conf.yaml")
+	if err != nil {
 		fmt.Println(err)
 	}
 	// 转为struct
-	err = yaml.Unmarshal(configs,&conf)
-	if err != nil{
+	err = yaml.Unmarshal(configs, &conf)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &conf
--- AutoByCity/main.go
+++ AutoByCity/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	el "github.com/xuri/excelize/v2"
 	"os"
 	"strconv"
 	"strings"
+
+	el "github.com/xuri/excelize/v2"
 )
 
 func main() {
@@ -13,9 +14,9 @@ func main() {
 	conf := ReadYaml()
 
 	// 转为数组
-	SourceSheetArr := strings.Split(conf.SourceSheets,",")
-	ToSheetsArr := strings.Split(conf.ToSheets,",")
-	SplitByForm := strings.Split(conf.SplitByForm,",")
+	SourceSheetArr := strings.Split(conf.SourceSheets, ",")
+	ToSheetsArr := strings.Split(conf.ToSheets, ",")
+	SplitByForm := strings.Split(conf.SplitByForm, ",")
 
 	if len(SourceSheetArr) != len(ToSheetsArr) {
 		fmt.Println("需要生成的sheet表，数量与上一个参数数量相同")
@@ -23,46 +24,46 @@ func main() {
 	}
 
 	// 打开主文件
-	f,err := el.OpenFile(conf.SourceData)
+	f, err := el.OpenFile(conf.SourceData)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// 生成文件
-	for k,sheet := range SourceSheetArr{
+	for k, sheet := range SourceSheetArr {
 		// 根据sheet表获取数据
-		rows,err := f.GetRows(sheet)
+		rows, err := f.GetRows(sheet)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		// 获取分割字段
-		index,err := strconv.Atoi(SplitByForm[k])
+		index, err := strconv.Atoi(SplitByForm[k])
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
 		// 将二维数组转为map
-		sheetMap,sheetHeader := rowsToMap(rows,index)
+		sheetMap, sheetHeader := rowsToMap(rows, index)
 		// 生成文件
-		MapToExcel(sheetMap,sheetHeader,ToSheetsArr[k])
+		MapToExcel(sheetMap, sheetHeader, ToSheetsArr[k])
 	}
 }
 
 // row 转为 map key地市 -> value [][]string
-func rowsToMap(rows [][]string,index int) (sheetMap map[string][][]string,header []string){
+func rowsToMap(rows [][]string, index int) (sheetMap map[string][][]string, header []string) {
 	sheetMap = make(map[string][][]string)
-	header = make([]string,0)
-	for k,row := range rows{
-		sheetArr  := make([][]string,0)
-		if k == 0{
-			header = append(header,row...)
-		}else{
+	header = make([]string, 0)
+	for k, row := range rows {
+		sheetArr := make([][]string, 0)
+		if k == 0 {
+			header = append(header, row...)
+		} else {
 			sheetArr = append(sheetArr, row)
 			if _, ok := sheetMap[row[index]]; ok {
-				sheetMap[row[index]] = append(sheetMap[row[index]],sheetArr...)
-			}else {
+				sheetMap[row[index]] = append(sheetMap[row[index]], sheetArr...)
+			} else {
 				sheetMap[row[index]] = sheetArr
 			}
 		}
@@ -71,14 +72,14 @@ func rowsToMap(rows [][]string,index int) (sheetMap map[string][][]string,header
 }
 
 // MapToExcel 将转为excel
-func MapToExcel(sheetMap map[string][][]string,header []string,sheet string)  {
-	for k,v := range sheetMap{
+func MapToExcel(sheetMap map[string][][]string, header []string, sheet string) {
+	for k, v := range sheetMap {
 		var f1 *el.File
-		flag := isExist("./resource/"+k+".xlsx")
-		if !flag{
+		flag := isExist("./resource/" + k + ".xlsx")
+		if !flag {
 			f1 = el.NewFile()
-		}else {
-			f1,_ = el.OpenFile("./resource/"+k+".xlsx")
+		} else {
+			f1, _ = el.OpenFile("./resource/" + k + ".xlsx")
 		}
 		index := f1.NewSheet(sheet)
 		f1.DeleteSheet("Sheet1")
@@ -86,13 +87,13 @@ func MapToExcel(sheetMap map[string][][]string,header []string,sheet string)  {
 		for k, v := range header {
 			f1.SetCellValue(sheet, axis(1, k), v)
 		}
-		for i := 0;i < len(v);i++{
+		for i := 0; i < len(v); i++ {
 			row := i + 2 // 从第二行开始
-			for k := 0; k < len(v[i]);k++ {
-				f1.SetCellValue(sheet,axis(row,k),v[i][k])
+			for k := 0; k < len(v[i]); k++ {
+				f1.SetCellValue(sheet, axis(row, k), v[i][k])
 			}
 		}
-		if err := f1.SaveAs("./resource/"+k+".xlsx"); err != nil {
+		if err := f1.SaveAs("./resource/" + k + ".xlsx"); err != nil {
 			fmt.Println(err)
 		}
 	}
@@ -102,23 +103,23 @@ func MapToExcel(sheetMap map[string][][]string,header []string,sheet string)  {
 func axis(row, column int) string {
 	if column <= 25 {
 		return fmt.Sprintf("%s%d", string(rune(65+column)), row)
-	} else if column > 25{
+	} else if column > 25 {
 		a := (column + 1) / (26)
 		column -= a * 26
 		code := string(rune(64 + a))
-		return fmt.Sprintf("%s%s%d",code,string(rune(65+column)),row)
+		return fmt.Sprintf("%s%s%d", code, string(rune(65+column)), row)
 	}
 	return ""
 }
 
 // isExist 判断文件是否存在
-func isExist(path string) bool{
+func isExist(path string) bool {
 	_, err := os.Stat(path)
-	if err != nil{
-		if os.IsExist(err){
+	if err != nil {
+		if os.IsExist(err) {
 			return true
 		}
-		if os.IsNotExist(err){
+		if os.IsNotExist(err) {
 			return false
 		}
 		fmt.Println(err)
